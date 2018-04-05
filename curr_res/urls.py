b4f9605769cd284b477e0d8ab48cb8477b732e57
ChristinaF <jabsrapunzel@hotmail.com>
2018-04-05 12:39:25 -0400

    Added Resident LogIn

--- curr_res/urls.py
+++ curr_res/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^logout/$', views.logout, name='logout'),
+
 ]
--- curr_res/views.py
+++ curr_res/views.py
@@ -1,7 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import loader
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.shortcuts import render
+from .forms import UserForm
+from .models import Tenant
 
 
 def index(request):
     template = loader.get_template('curr_res/base_curr_res.html')
     return HttpResponse(template.render(request))
+
+
+def logout(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'curr_res/login.html', context)
+
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                tenants = Tenant.objects.filter(user=request.user)
+                return render(request, 'curr_res/base_curr_res.html', {'tenants': tenants})
+            else:
+                return render(request, 'curr_res/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'curr_res/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'curr_res/login.html')
